3494d29c968e10999bee9482ca07271734c252f7
Paul WOISARD <paulwoisard@gmail.com>
2022-05-19 23:51:58 +0200

    protection media sous macOS

--- pages/Misiyon.php
+++ pages/Misiyon.php
@@ -64,7 +64,7 @@
         <br>
         <div class="song_title" id="song_title" style="border:2px solid #000;width: 40%;cursor: pointer;padding-bottom: 1%; margin-left: 30%; background-color: #D88851;">► Chanson 01 - West Indies Blue</div>
         <br><br>
-        <audio id="audio" controls autoplay controlsList="nodownload"  oncontextmenu="return false">
+        <audio id="audio" controls autoplay controlsList="nodownload" oncontextmenu="return false">
           <source src="../assets/audio/Misiyon/1.mp3" type="audio/mpeg" />
         </audio>
         <br><br>
