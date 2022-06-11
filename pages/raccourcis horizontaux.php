61898842a60b356e912336c9470234e036143444
Paul WOISARD <paulwoisard@gmail.com>
2022-06-11 16:42:48 +0200

    Mise à jour des Raccourcis

--- pages/raccourcis horizontaux.php
+++ pages/raccourcis horizontaux.php
@@ -6,133 +6,133 @@
     <div class="raccourcis-box">
       <a href="../pages/WIB.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">WIB (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/WIB.png" alt="" />
+        <img src="../assets/img/miniatureV2/WIB.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/DLO.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">DLO (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/DLO.png" alt="" />
+        <img src="../assets/img/miniatureV2/DLO.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Fòs é kouraj.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Fòs é kouraj (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Fòs é kouraj.png" alt="" />
+        <img src="../assets/img/miniatureV2/Fòs é kouraj.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Piétònn'la (Philippe BLAZE).php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Piétònn'la (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Piétònn'la (Philippe BLAZE).png" alt="" />
+        <img src="../assets/img/miniatureV2/Piétònn'la (Philippe BLAZE).png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Sò a limanité.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Sò a limanité (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Sò a limanité.png" alt="" />
+        <img src="../assets/img/miniatureV2/Sò a limanité.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Sonjé BINO (Philippe BLAZE).php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Sonjé BINO (vidéo)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Sonjé BINO (Philippe BLAZE).png" alt="" />
+        <img src="../assets/img/miniatureV2/Sonjé BINO (Philippe BLAZE).png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/LILY.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Lily de Pierre PERRET<br>Vidéo en Live</h2>
-        <img src="../assets/img/miniature/LILY.png" alt="" />
+        <img src="../assets/img/miniatureV2/LILY.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Petite_Marie.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Petite Marie de CABREL<br>Vidéo en Live</h2>
-        <img src="../assets/img/miniature/Petite_Marie.png" alt="" />
+        <img src="../assets/img/miniatureV2/Petite_Marie.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Nou_mélé_Philippe_BLAZE.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Nou mélé (Philippe BLAZE)<br>Vidéo en Live</h2>
-        <img src="../assets/img/miniature/Nou_mélé_Philippe_BLAZE.png" alt="" />
+        <img src="../assets/img/miniatureV2/Nou_mélé_Philippe_BLAZE.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Chanson_Hommage_aux_Gwo_Mòdan.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Hommage à 3 Gwo-Mòdan<br>Vidéo en Live</h2>
-        <img src="../assets/img/miniature/Chanson_Hommage_aux_Gwo_Mòdan.png" alt="" />
+        <img src="../assets/img/miniatureV2/Chanson_Hommage_aux_Gwo_Mòdan.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Philippe_BLAZE_piano_bar.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Piano-bar<br>Vidéo en Live</h2>
-        <img src="../assets/img/miniature/Philippe_BLAZE_piano_bar.png" alt="" />
+        <img src="../assets/img/miniatureV2/Philippe_BLAZE_piano_bar.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Ekzòd.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Ekzòd (vidéo)<br>Reggae</h2>
-        <img src="../assets/img/miniature/Ekzòd.png" alt="" />
+        <img src="../assets/img/miniatureV2/Ekzòd.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Avé Maria Antillais.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Avé Maria Antillais (vidéo)<br>Ballade Créole</h2>
-        <img src="../assets/img/miniature/Avé Maria Antillais.png" alt="" />
+        <img src="../assets/img/miniatureV2/Avé Maria Antillais.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Pénélope.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Pénélope (vidéo)<br>Salsa</h2>
-        <img src="../assets/img/miniature/Pénélope.png" alt="" />
+        <img src="../assets/img/miniatureV2/Pénélope.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Konjé bonifié.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Konjé bonifié (vidéo)<br>Biguine</h2>
-        <img src="../assets/img/miniature/Konjé bonifié.png" alt="" />
+        <img src="../assets/img/miniatureV2/Konjé bonifié.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Mal tèt.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Mal tèt (titre audio)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Mal tèt.png" alt="" />
+        <img src="../assets/img/miniatureV2/Mal tèt.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Eritaj.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Eritaj (extrait de l'album)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Eritaj.png" alt="" />
+        <img src="../assets/img/miniatureV2/Eritaj.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Konsyans.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Konsyans (album complet)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Konsyans.png" alt="" />
+        <img src="../assets/img/miniatureV2/Konsyans.png" alt="" />
       </a>
     </div>
 
     <div class="raccourcis-box">
       <a href="../pages/Misiyon.php" style="text-decoration:none;">
         <h2 class="liens-raccourcis">Misiyon (album complet)<br>Gwoka évolutif</h2>
-        <img src="../assets/img/miniature/Misiyon.png" alt="" />
+        <img src="../assets/img/miniatureV2/Misiyon.png" alt="" />
       </a>
     </div>
   </div>
