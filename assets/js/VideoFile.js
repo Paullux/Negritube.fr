27ff93aa20c013cbf2975622820914bbe2389c41
Paul WOISARD <paulwoisard@gmail.com>
2022-07-05 07:32:20 +0200

    gestion redirection en js

--- assets/js/VideoFile.js
+++ assets/js/VideoFile.js
@@ -103,10 +103,10 @@ var isMobile = {
 var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var oldTrack = urlParams.get('track');
-var track = document.location.toString().split("-")[1].split(".")[0];
+var track = oldTrack;
 
-if (oldTrack != null) {
-  track = oldTrack;
+if (oldTrack == null) {
+  track = document.location.toString().split("-")[1].split(".")[0];;
 }
 
 window.addEventListener("load", function(event) {
