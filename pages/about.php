4967852c8dd728f54f4df48fb108d0b63343d32d
Paul WOISARD <paulwoisard@gmail.com>
2022-07-31 15:35:06 +0200

    Ajout nouveaux document venant de philippe

---
+++ pages/about.php
@@ -0,0 +1,239 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <!-- HTML Meta Tags -->
+  <title>A propos de Philippe BLAZE</title>
+  <meta name="description" content="Pour (re-)découvrir le gwoka, musique de Guadeloupe.">
+  <meta name="keywords" content="créole" />
+  <meta name="author" content="Philippe Blaze" />
+  <meta name="theme-color" content="#f6b73c" />
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://negritube.fr/about.html">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="A propos de Philippe BLAZE">
+  <meta property="og:description" content="pour (re-)découvrir le gwoka, musique de Guadeloupe.">
+  <meta property="og:image" content="https://negritube.fr/assets/img/Carte-og.png">
+  <meta property="og:locale" content="fr_FR" />
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary" />
+  <meta property="twitter:domain" content="negritube.fr">
+  <meta property="twitter:url" content="https://negritube.fr/about.html">
+  <meta name="twitter:title" content="A propos de Philippe BLAZE">
+  <meta name="twitter:description" content="pour (re-)découvrir le gwoka, musique de Guadeloupe.">
+  <meta name="twitter:image" content="https://negritube.fr/assets/img/Carte-Twitter.png">
+  <meta name="twitter:image:alt" content="Negritube.fr" />
+
+  <link rel="icon" type="image/png" href="assets/img/favicon.png" />
+  <link href="../assets/css/styles.css" rel="stylesheet" />
+  <link href="../assets/css/switchTheme.css" rel="stylesheet" />
+  <link href="../assets/css/dark.css" rel="stylesheet" id="stylesheet" />
+  <link href="../assets/css/index.css" rel="stylesheet" />
+  <link href="../assets/css/affiche.css" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+  <script src="../assets/js/anim.js" crossorigin="paulw"></script>
+  <script src="../assets/js/switchTheme.js" crossorigin="paulw"></script>
+  <script>
+    if (window.location.href.indexOf("paulluxwaffle.synology.me") > -1) {
+      Server = "https://paulluxwaffle.synology.me/Multi-Plateform/about.html";
+    } else {
+      Server = "https://negritube.fr/about.html";
+    }
+    window.history.replaceState('', '', Server);
+  </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K07Z7YG6ZX"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-K07Z7YG6ZX');
+  </script>
+
+  <!-- Clarity tracking code for https://negritube.fr/ -->
+  <script>
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "c73v5s2zme");
+  </script>
+
+  <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="71bb40dd-296e-4604-8a7b-f47916d5ad6a" data-blockingmode="auto" type="text/javascript"></script>
+
+  <!-- Matomo -->
+  <script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.negritube.fr"]);
+  _paq.push(["setDomains", ["*.negritube.fr"]]);
+  _paq.push(["setDoNotTrack", true]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//negritube.fr/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+  </script>
+  <noscript><p><img src="//negritube.fr/matomo/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p></noscript>
+  <!-- End Matomo Code -->
+</head>
+<body>
+  <div id="turnDeviceNotification"></div>
+    <div class="index" style="align-items:center;">
+      <div class="big-title">
+        <div id="item">
+          <a href="."><img class="logo" src="assets/img/logo-negritube.png" alt="negritube"></a>
+        </div>
+        <div id="item">
+          <a href="." style="text-decoration: none; ">
+            <h1 class="title_site">
+              Negritube.fr
+            </h1>
+          </a>
+          <h2 class="subtitle_site">
+            Pour (re-)découvrir le Gwoka,
+            <br>une musique de Guadeloupe !
+          </h2>
+        </div>
+        <div class="container" id="item">
+          <h3>Changement de Thème</h3>
+          <label id="switch" class="switch">
+            <input type="checkbox" onchange="toggleTheme()" id="slider">
+            <span class="slider round"></span>
+          </label>
+          <!-- <button id="switch" onclick="toggleTheme()">Switch</button> -->
+        </div>
+      </div>
+      <nav>
+        <div class="Bouton_Menu" onclick="open_menu();">
+          <img id="lebouton" src="assets/img/Menu.png" alt="Menu">
+          <p>
+            Menu
+          </p>
+        </div>
+        <div id="Menu_Cache">
+          <li><a href="video-0.html">Les Clips</a></li>
+          <li><a href="audio-1.html">Les Albums</a></li>
+          <li><a href="contact.html">Negritube.fr - Prise de Contact</a></li>
+          <li><a href="about.html">Negritube.fr - A propos de Philippe BLAZE</a></li>
+        </div>
+      </nav>
+      <div class="space">
+        <img class="about" src="assets/img/about/logo-prod.jpg" alt="logo_prod">
+        <div class="center" >Philippe BLAZE
+          <br>
+          Auteur - Compositeur - Interprète – Guitariste
+          <img class="about" src="assets/img/about/photo_de_philippe.jpg" alt="philippe">
+
+          <br>
+
+          <br>Tél : 0690 27 55 18
+          <br>Email : blazephilippe@gmail.com
+          <br>Site : negritube.fr
+          <br>Facebook – Youtube
+
+          <br>
+
+          <br>BIO EXPRESS
+          <br>
+          <br><>  A l’âge de 7 ans Philippe BLAZE quitte la Guadeloupe pour Paris
+          <br>
+          <br>Ecole de musique – Paris :
+          <br>Guitare et solfège
+          <br>Conservatoire Régional de Loire Atlantique – Nantes :			Chant classique et solfège
+          <br>
+          <br><>  A 25 ans : retour en Guadeloupe
+          <br>
+          <br>Professeur de musique : guitare, chant, solfège, basse
+          <br>
+          <br>PARCOURS MUSICAL
+          <br>
+          <br>1982	:	Création de l’Association de CAAPARE « Comité d’Action et
+          <br>d’Assistance Promotionnelle des Artistes Rock et de leurs
+          <br>Enregistrements »
+          <br>Membres fondateurs :
+          <br>• Directeur Général : Bruno LION (un des pontes actuels de la SACEM)
+          <br>• Directeur artistique : Blaise MASSON à l’époque le plus jeune
+          <br>producteur de France (LUKRATE MILK)
+          <br>•      Président : Michel SANDRA
+          <br>•      Secrétaire général : Philippe BLAZE
+          <br>
+          <br>1983 :		Production d’albums (vinyl) des groupes :
+          <br>•      CONTACT (Rock New Wave)
+          <br>•      ICE (Cold Wave)
+          <br>Participation aux concerts « Hommage à MARLEY »
+          <br>
+          <br>NANTES
+          <br>1984-1987 :	Concert avec les musiciens d’APARTHEID NOT
+          <br>Chanteur dans MAX COMBO
+          <br>Percussionniste dans AYOKA
+          <br>Chanteur Bassiste dans PHIL MUSIC
+          <br>
+          <br>GUADELOUPE
+          <br>1990-1996 :	Diverses prestations live :
+          <br>Guitariste avec Guy CORDOVAL
+          <br>Choriste avec KAFé, TI-FABRICE, KATOURé
+          <br>Bassiste avec LALLOUé JAH
+          <br>Chanteur avec José MANCLIERE
+          <br>Auteur & co-auteur sur l’album de TI-FABRICE
+          <br>Production Hibiscus Record (Martinique)
+          <br>
+          <br>1996	:	Création de PAPILLON PROD et Publication du CD BLAZE « Matouba »
+          <br>Réf. PAP 001
+          <br>
+          <br>1998 :		Production du CD JÒD’LA « Konsyans »
+          <br>Réf. PAP 002
+          <br>
+          <br>2002 :		Réédition du CD BLAZE « Matouba »
+          <br>Réf. ML 001
+          <br>
+          <br>1997-2014 :	Leader du groupe JÒD’LA (Gwo Ka évolutif)
+          <br>Participation à différents concerts et festivals en Guadeloupe
+          <br>
+
+          <img class="about" src="assets/img/about/photo_concert.jpg" alt="concert">
+          Prestation Festival de Gwo Ka Sainte-Anne
+
+          <br>
+          <br>2008 :		Production du single JÒD’LA 3Festival Téyat Zabim »
+          <br>Réf. PAP 003
+          <br>Tournée « Intercom Tour »
+          <br>
+          <br>2011 :		Production du CD JÒD’LA « Misiyon »
+          <br>Réf. PAP 004
+          <br>
+          <br>2012-2020 :	Participation et réalisation de divers projets d’albums :
+          <br>• K.LI.K.
+          <br>• SWEETNESS
+          <br>• KA FèTAN
+          <br>• KARUKERA BèLè
+          <br>• Etc…
+          <br>
+          <img class="about" src="assets/img/about/photo_concert_2.jpg" alt="concert_2">
+          <br>Prestation Centre Félix Proto - Abymes
+          <br>
+          <br>2020 :		Album Philippe BLAZE « Eritaj »
+          <br>Réf. PAP 006
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<footer class="footerIndex">
+  <p>All rights reserved © Negritube.fr | Music and Video by Philippe Blaze | Site by Paullux Waffle</p>
+</footer>
+<script src="../assets/js/menu.js" crossorigin="paulw"></script>
+<script src="../assets/js/landscapeWarning.js" crossorigin="paulw"></script>
+</html>
