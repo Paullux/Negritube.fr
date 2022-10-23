784009da0ad2b44f1182e24a5e306565dd5a3e00
Paul WOISARD <paulwoisard@gmail.com>
2022-10-23 13:43:37 +0200

    Ajout AdsSense

--- pages/VideoFile.php
+++ pages/VideoFile.php
@@ -123,6 +123,8 @@ $ogLink =  rel2abs($csv[$track][4], "https://www.negritube.fr/pages/");
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-K07Z7YG6ZX"></script>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2838504479669283"
+     crossorigin="anonymous"></script>
   <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
