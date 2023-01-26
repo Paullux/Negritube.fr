6eb9bde8ca249829db765ff41b5df6463b4edbe3
Paul WOISARD <paulwoisard@gmail.com>
2023-01-26 07:43:52 +0100

    Amélioration de l'animation de l'index

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,15 @@ let miniatureCurrentImageIndex = 0;
 var cover = document.getElementById("Covers");
 var coverCurrentImageIndex = 0;
 
-miniature.style.backgroundImage = `url('assets/img/miniature/1.png')`;
+miniature.style.backgroundImage = `url('assets/img/miniature/6.png')`;
 cover.style.backgroundImage = `url('assets/img/album cover/Mal_tèt.jpg')`;
 
 setInterval(() => {
   miniature.style.backgroundImage = `url("${window.Miniatures[miniatureCurrentImageIndex]}")`;
   miniatureCurrentImageIndex = (miniatureCurrentImageIndex + 1) % window.Miniatures.length;
-
-  cover.style.backgroundImage = `url("${window.Covers[coverCurrentImageIndex]}")`;
-  coverCurrentImageIndex = (coverCurrentImageIndex + 1) % window.Covers.length;
 }, 6000);
+
+setInterval(() => {
+cover.style.backgroundImage = `url("${window.Covers[coverCurrentImageIndex]}")`;
+coverCurrentImageIndex = (coverCurrentImageIndex + 1) % window.Covers.length;
+}, 4500);
