ffedc4195b7e5e16c740b10141905a05e365ca29
Paul WOISARD <paulwoisard@gmail.com>
2023-01-27 08:14:36 +0100

    travail sur les images de fond

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,11 +8,21 @@ miniature.style.backgroundImage = `url('assets/img/miniature/6.png')`;
 cover.style.backgroundImage = `url('assets/img/album cover/Mal_tèt.jpg')`;
 
 setInterval(() => {
-  miniature.style.backgroundImage = `url("${window.Miniatures[miniatureCurrentImageIndex]}")`;
+  const imgMiniature = new Image();
+  imgMiniature.src = window.Miniatures[miniatureCurrentImageIndex];
+  imgMiniature.onload = function () {
+    miniature.style.backgroundImage = `url("${window.Miniatures[miniatureCurrentImageIndex]}")`;
+    miniature.style.transition = `background 3s linear;`;
+  }
   miniatureCurrentImageIndex = (miniatureCurrentImageIndex + 1) % window.Miniatures.length;
 }, 6000);
 
 setInterval(() => {
-cover.style.backgroundImage = `url("${window.Covers[coverCurrentImageIndex]}")`;
-coverCurrentImageIndex = (coverCurrentImageIndex + 1) % window.Covers.length;
-}, 4500);
+  const imgCover = new Image();
+  imgCover.src = window.Covers[coverCurrentImageIndex];
+  imgCover.onload = function () {
+	cover.style.backgroundImage = `url("${window.Covers[coverCurrentImageIndex]}")`;
+	cover.style.transition = `background 3s linear;`;
+  }
+  coverCurrentImageIndex = (coverCurrentImageIndex + 1) % window.Covers.length;
+}, 4000);
